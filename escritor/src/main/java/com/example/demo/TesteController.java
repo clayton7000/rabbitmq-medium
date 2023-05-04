7f01d270e3e35a9eddd27ba54cba819baf07ce50
clayton7000 <clayton_700_700@hotmail.com>
2023-05-04 11:57:53 -0300

    first commit

---
+++ escritor/src/main/java/com/example/demo/TesteController.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/teste")
+public class TesteController {
+
+    public TesteController(AmqpTemplate queueSender) {
+        this.queueSender = queueSender;
+    }
+
+    private final AmqpTemplate queueSender;
+
+    @GetMapping
+    public String send(){
+        queueSender.convertAndSend("teste-exchange", "routing-key-teste", "test message");
+        return "ok. done";
+    }
+
+}
